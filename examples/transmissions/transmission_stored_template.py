803934e6b8cf2e330ff89205c960cdba930fbda1
Michael Timbrook <timbrook480@gmail.com>
2017-02-11 14:21:43 -0500

    Cleaned up repo a bit

--- examples/transmissions/transmission_stored_template.py
+++
@@ -1,9 +0,0 @@
-from sparkpost import SparkPost
-
-sp = SparkPost()
-
-response = sp.transmissions.send(
-    recipients=['you@me.com'],
-    template='my-template-id',
-    use_draft_template=True
-)
