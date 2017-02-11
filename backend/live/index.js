0db59059622fcfbf8516797df6915b81d6bc9437
Michael Timbrook <timbrook480@gmail.com>
2017-02-11 18:22:08 -0500

    She speaks

--- backend/live/index.js
+++ backend/live/index.js
@@ -4,6 +4,12 @@ module.exports = (io) => {
     debug('Loaded');
     io.of('/api/message')
         .on('connection', (socket) => {
-            socket.emit('message', 'Hello my fellow shoe lover');
+            socket.emit('message', 'Hello Shoe Person, Say something.');
+            socket.emit('control', { listen: true });
+            socket.on('message', (message) => {
+                debug(message);
+                socket.emit('control', { listen: false });
+                socket.emit('message', 'Thanks, you said ' + message);
+            });
         });
 };
--- frontend/app/js/actions/messages.js
+++ frontend/app/js/actions/messages.js
@@ -1,28 +1,52 @@
 import * as io from 'socket.io-client';
+import store from '../store';
+import { START_LISTENING, STOP_LISTENING } from './listen';
 
 export const BOT = 'BOT';
 export const HUMAN = 'HUMAN';
 export const BOT_MESSAGE = 'BOT_MESSAGE';
 export const HUMAN_MESSAGE = 'HUMAN_MESSAGE';
 
+const socket = io('http://localhost:3000/api/message');
+
+socket.on('connect', () => {
+    console.log('Yay!');
+});
+
+socket.on('message', (msg) => {
+  store.dispatch(robotMessage(msg));
+});
+
+socket.on('control', (msg) => {
+  if (msg.listen) {
+    store.dispatch({
+      type: START_LISTENING,
+    });
+  } else {
+    store.dispatch({
+      type: STOP_LISTENING,
+    });
+  }
+});
+
+function robotMessage(message) {
+  return (dispatch) => {
+    const msg = new SpeechSynthesisUtterance(message);
+    window.speechSynthesis.speak(msg);
+    return dispatch({
+      type: BOT_MESSAGE,
+      payload: message
+    });
+  };
+}
+
 export function humanMessage(message) {
   return (dispatch) => {
     console.log(message);
+    socket.emit('message', message);
     return dispatch({
       type: HUMAN_MESSAGE,
       payload: message,
     });
   };
 }
-
-const socket = io('http://localhost:3000/api/message');
-
-socket.on('connect', () => {
-    console.log('Yay!');
-
-});
-
-socket.on('message', (msg) => {
-  console.log('data');
-  console.log(msg);
-});
