35d2df35089ffa19cd2648ca0c5201e7a376ee7c
Michael Timbrook <timbrook480@gmail.com>
2017-02-11 17:59:39 -0500

    Show comms

--- backend/live/index.js
+++ backend/live/index.js
@@ -1,3 +1,9 @@
+var debug = require('debug')('backend:live');
+
 module.exports = (io) => {
-    
+    debug('Loaded');
+    io.of('/api/message')
+        .on('connection', (socket) => {
+            socket.emit('message', 'Hello my fellow shoe lover');
+        });
 };
