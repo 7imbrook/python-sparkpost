1d4f99d899871260d9852da02d60833ceea1f9d7
Michael Timbrook <timbrook480@gmail.com>
2017-02-12 14:48:08 -0500

    Shhhhhhhh

--- frontend/app/js/actions/messages.js
+++ frontend/app/js/actions/messages.js
@@ -55,6 +55,8 @@ function goodCodeBlock(message, dispatch, getState) {
 }
 
 function betterCodeBlock(message, dispatch, getState) {
+  if (!getState().messages && getState().messages[getState().messages.length - 1].content == message)
+    return;
   const ready = getState().splash;
   if (!ready) {
     if (message.messageFormat === PLAINTEXT_MESSAGE) {
