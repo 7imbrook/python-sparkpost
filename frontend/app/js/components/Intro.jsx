b6a7ff0cc33f121043cc04a81a649b33d780f90d
Melissa Young <melissay94@yahoo.com>
2017-02-12 13:17:12 -0500

    Fixed english for intro paragraph on splashscreen

--- frontend/app/js/components/Intro.jsx
+++ frontend/app/js/components/Intro.jsx
@@ -15,8 +15,8 @@ class Intro extends React.Component {
           <img className="intro-logo" src={Logo} alt="logo" />
           <p className="intro-tagline">Helping with sneakers since 2017</p>
           <p className="intro-pitch">SneakSpeak was designed to assist its users in their day to day when it comes
-            to their footwear. Whether you&apos;re trying to picking out what to kicks to pick or searching for
-            something new and rad, our product is the way to go.
+            to footwear. Whether you&apos;re trying to pick out what kicks to wear or searching for
+            something new and rad, our product is the way to go!
           </p>
           <button className="intro-button" onClick={this.props.changeView}>Let&apos;s Go!</button>
         </div>
