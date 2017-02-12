ed764fe9d53c7e5f598e014c2cbb7239d26c72b5
Dylan Vuz <vuzdylan@gmail.com>
2017-02-11 21:46:08 -0500

    temp

--- frontend/app/js/components/Message.jsx
+++ frontend/app/js/components/Message.jsx
@@ -4,8 +4,8 @@ import '../../scss/message.scss';
 
 const Message = props => (
   <div className={`message ${props.from === BOT ? 'bot' : 'human'}`}>
+    <div className="from">{props.from === BOT ? 'Sneak Advisor' : 'Joey D from Long Island'}</div>
     <div className="body">
-      <div className="from">{props.from === BOT ? 'Sneak Advisor' : 'Joey D from Long Island'}</div>
       <div className="content">{props.content}</div>
       <div className="timeStamp">{props.timeStamp}</div>
     </div>
@@ -16,12 +16,6 @@ Message.propTypes = {
   from: React.PropTypes.string.isRequired,
   content: React.PropTypes.string.isRequired,
   timeStamp: React.PropTypes.string.isRequired,
-};
-
-Message.defaultProps = {
-  from: BOT,
-  content: 'dank memer',
-  timeStamp: 'feb 31 25:00 pm',
 };
 
 export default Message;
--- frontend/app/js/components/Messages.jsx
+++ frontend/app/js/components/Messages.jsx
@@ -1,11 +1,70 @@
 import React from 'react';
 import Message from './Message';
+import { BOT, HUMAN } from '../actions/messages';
 
-const Messages = () => (
-  <div>
-    Messages
-    <Message />
-  </div>
-);
+const tempMessages = [
+  {
+    from: BOT,
+    content: 'This is message one',
+    timeStamp: 'Feb 31 25:00pm',
+  },
+  {
+    from: HUMAN,
+    content: 'This is message two',
+    timeStamp: 'Feb 31 25:01pm',
+  },
+  {
+    from: BOT,
+    content: 'This is message three',
+    timeStamp: 'Feb 31 25:02pm',
+  },
+  {
+    from: HUMAN,
+    content: 'This is message four',
+    timeStamp: 'Feb 31 25:03pm',
+  },
+  {
+    from: BOT,
+    content: 'This is message five',
+    timeStamp: 'Feb 31 25:04pm',
+  },
+  {
+    from: HUMAN,
+    content: 'This is message six',
+    timeStamp: 'Feb 31 25:05pm',
+  },
+  {
+    from: BOT,
+    content: 'This is message seven',
+    timeStamp: 'Feb 31 25:06pm',
+  },
+  {
+    from: HUMAN,
+    content: 'This is message eight',
+    timeStamp: 'Feb 31 25:07pm',
+  },
+];
+
+class Messages extends React.Component {
+  componetDidMount() {
+    this.scroll.scrollTop = this.scroll.scrollHeight;
+  }
+
+  componetDidUpdate() {
+    this.scroll.scrollTop = this.scroll.scrollHeight;
+  }
+
+  render() {
+    const messages = tempMessages.map(message => (
+      <Message key={message.content} {...message} />
+    ));
+
+    return (
+      <div ref={(c) => { this.scroll = c; }} className="messages">
+        {messages}
+      </div>
+    );
+  }
+}
 
 export default Messages;
