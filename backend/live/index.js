99dabf9484f0b1dc376440906ec0f00f02bf9029
Michael Timbrook <timbrook480@gmail.com>
2017-02-12 02:13:36 -0500

    Temp

--- backend/live/index.js
+++ backend/live/index.js
@@ -1,31 +1,61 @@
 const debug = require('debug')('backend:live'),
-      natural = require('natural')
+      natural = require('natural'),
+      NGrams = natural.NGrams,
+      metaphone = natural.Metaphone,
+      fs = require('fs');
 ;
 
-const tokenizer = new natural.WordTokenizer();
-natural.LancasterStemmer.attach();
+const tokenizer = new natural.WordTokenizer(),
+      nounInflector = new natural.NounInflector()
+;
+
+// Load in brands
+const brands = fs.readFileSync(__dirname + '/../data/brands.txt')
+    .toString('utf8')
+    .split('\n')
+    .filter(b => b)
+    .map(b => nounInflector.singularize(b));
 
 function handleMessage(message) {
     const striped = message.replace(/'/, '');
-    
-    const tokens = striped.tokenizeAndStem();
-    debug(tokens);
+    const tokens = tokenizer.tokenize(striped);
+
+    const found_brands = brands.filter(brand => {
+        return tokens
+            .map(token => natural.JaroWinklerDistance(nounInflector.singularize(token), brand))
+            .reduce((acc, cur) => acc || cur > 0.9, false);
+    });
+
+    if (found_brands.length > 0) {
+        return found_brands;
+    }
+
+    // rank
+    return 'I didn\'t understand that';
 }
 
 function onConnection(socket) {
     debug('Connected user ' + socket.id);
     // socket.emit('message', 'Welcome to Sneak Speak.');
     // socket.emit('message', 'Say something');
-    socket.on('message', handleMessage);
+    socket.on('message', (msg) => {
+        const response = handleMessage(msg);
+        debug(response);
+        socket.emit('message', response);
+    });
 
     // Test classes
-    handleMessage('What shoes should I wear today?');
-    handleMessage('How are my shoes?');
-    handleMessage('I want to run today.');
-    handleMessage('I\'m going for a run today');
-    handleMessage('I\'ve got a date later');
-    handleMessage('Whats new?');
-    handleMessage('Let\'s go old school today');
+    [
+        'What shoes should I wear today?',
+        'How are my shoes?',
+        'I want to run today.',
+        'I\'m going for a run today',
+        'I\'ve got a date later',
+        'Whats new?',
+        'Let\'s go old school today',
+        'What vans should I wear?',
+        'Show me vans.',
+    ].forEach((test) => debug(handleMessage(test)));
 }
 
 module.exports = (io) => {
